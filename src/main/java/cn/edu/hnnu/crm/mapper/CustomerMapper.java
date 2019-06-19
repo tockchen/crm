c398b90b8ba69e62f9d36c5e1e4481a2ec33c7bf
tockchen <1162797060@qq.com>
2019-06-19 23:52:51 +0800

    添加客户详细信息模块

--- src/main/java/cn/edu/hnnu/crm/mapper/CustomerMapper.java
+++ src/main/java/cn/edu/hnnu/crm/mapper/CustomerMapper.java
@@ -17,7 +17,6 @@ public interface CustomerMapper {
     void delete(Integer id);
     @Select("select * from customer where ((cust_source = #{custSource}) or (cust_industry = #{custIndustry}) or (cust_level = #{custLevel}) or (cust_name like CONCAT('%',#{custName},'%'))) " )
     List<Customer> likeSelect(String custName,String custSource,String custIndustry,String custLevel);
-
     @Update("update customer set cust_name=#{cust_name},cust_source=#{cust_source},cust_industry=#{cust_industry},cust_level=#{cust_level},cust_linkman=#{cust_linkman},cust_phone=#{cust_phone},cust_mobile=#{cust_mobile},cust_zipcode=#{cust_zipcode},cust_address=#{cust_address} where cust_id=#{cust_id}")
     void update(Customer customer);
 
